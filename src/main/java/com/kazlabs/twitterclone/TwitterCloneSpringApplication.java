55e1cde49d275852da425e907bf8a83cd4ff05c5
Kaz <kaz@kaaaz.eu>
2023-08-21 00:31:41 +0200

    Initial commit

---
+++ src/main/java/com/kazlabs/twitterclone/TwitterCloneSpringApplication.java
@@ -0,0 +1,13 @@
+package com.kazlabs.twitterclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterCloneSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterCloneSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kazlabs/twitterclone/TwitterCloneSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kazlabs.twitterclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterCloneSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
